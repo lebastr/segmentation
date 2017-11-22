432bb359f53352b0830869e7db04842398c2afdf
Alexander <lebastr@gmail.com>
2017-11-22 20:16:40 +0300

    добавил инициализацию весов из vgg16

--- unet.py
+++ unet.py
@@ -62,6 +62,37 @@ class UnetModel(Model):
         super(UnetModel, self).__init__(inputs=inputs, outputs=conv10, name='unet')
 #        self.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy')
 
+def create_VGG16_conv_pipe(img_input):
+    # Block 1
+    net = {}
+
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    net['b1'] = x
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+
+    # Block 2
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+    net['b2'] = x
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+
+    # Block 3
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+    net['b3'] = x
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+
+    # Block 4
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+    net['b4'] = x
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+    net['to-unet'] = x
+    return net
+
 class VGGUnetModel(Model):
     def __init_vgg_unet__(self, labels=2):
         def two_conv2d(name, n_filter1, n_filter2, x):
@@ -75,23 +106,30 @@ class VGGUnetModel(Model):
             x = two_conv2d(name + "two_conv_block", n_filter, n_filter // 2, x)
             return Concatenate()([UpSampling2D(size=(2, 2))(x), y])
 
-        vgg = keras.applications.vgg16.VGG16(include_top=True, weights='imagenet', input_tensor=None,
-                                             input_shape=None, pooling=None, classes=1000)
+        #vgg = keras.applications.vgg16.VGG16(include_top=True, weights='imagenet', input_tensor=None,
+        #                                     input_shape=None, pooling=None, classes=1000)
 
-        [b1, b2, b3, b4, x] = [vgg.layers[i].output for i in [2, 5, 9, 13, 14]]
+        input_img = Input(shape=(224, 224, 3))
+        vgg_pipe = create_VGG16_conv_pipe(BatchNormalization(name="input_batch_norm")(input_img))
 
-        x = conv_concate_block("cc1", 1024, x, b4)
-        x = conv_concate_block("cc2", 512, x, b3)
-        x = conv_concate_block("cc3", 256, x, b2)
-        x = conv_concate_block("cc4", 128, x, b1)
+        x = conv_concate_block("cc1", 1024, vgg_pipe['to-unet'], vgg_pipe['b4'])
+        x = conv_concate_block("cc2", 512, x, vgg_pipe['b3'])
+        x = conv_concate_block("cc3", 256, x, vgg_pipe['b2'])
+        x = conv_concate_block("cc4", 128, x, vgg_pipe['b1'])
 
         x = two_conv2d("last_conv_block", 64, 64, x)
-        x = Conv2D(labels, (1, 1), activation='softmax', padding='same', name="sigmoid_conv")(x)
+        x = Conv2D(labels, (1, 1), activation='sigmoid', padding='same', name="sigmoid_conv")(x)
 
-        self.vgg_layers = [vgg.layers[i] for i in range(1, 14)]
-
-        super(VGGUnetModel, self).__init__(inputs=vgg.input, outputs=x, name='unet')
+        super(VGGUnetModel, self).__init__(inputs=input_img, outputs=x, name='vgg-unet')
+        self.vgg_layers = [self.layers[i] for i in range(2,15)]
 
     def __init__(self):
         self.u_history = []
         self.__init_vgg_unet__()
+
+    def load_vgg_weights(self):
+        vgg = keras.applications.vgg16.VGG16(include_top=False, weights='imagenet', input_tensor=None,
+                                             input_shape=None, pooling=None, classes=1000)
+
+        for i, layer in enumerate(vgg.layers[1:14]):
+            self.vgg_layers[i].set_weights(layer.get_weights())
