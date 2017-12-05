dd99ed99aaf4aa3ed0844cb815fdbf87f7a48fe2
Alexander Lebedev <lebastr@gmail.com>
2017-12-05 21:55:11 +0000

    скрипт работает

--- src/train.py
+++ src/train.py
@@ -16,13 +16,15 @@ from tensorboard import SummaryWriter
 
 
 class NManager(object):
-    def __init__(self, model_dir):
-        self.model_dir = model_dir
-        self.model_state_path = model_dir + "/state.json"
+    def __init__(self, root, name):
+        self.model_dir = root + "/" + name
+        self.model_state_path = self.model_dir + "/state.json"
+        self.name = name
 
         if not os.path.exists(self.model_dir):
             os.mkdir(self.model_dir)
-
+            print("Net will be live in %s" % self.model_dir)
+            
         if os.path.exists(self.model_state_path):
             self.restore()
             self.registered = True
@@ -45,19 +47,19 @@ class NManager(object):
         self.name = state['name']
 
         self.net = torch.load(self.__last_model_path__())
+        print("Net %s was restored from checkpoint %d" % (self.name, self.iteration))
         return self.net
 
-    def register_net(self, net, name):
+    def register_net(self, net):
         assert not self.registered, "Network is already registered"
         assert isinstance(net, U.Unet), "Net must be Unet instance"
-        assert isinstance(name, str), "name must be string"
 
         self.iteration = 0
-        self.name = name
         self.net = net
         self.registered = True
 
     def save(self):
+        print("\ncheckpoint at: %d" % self.iteration)
         json.dump({'iteration': self.iteration, 'name': self.name}, open(self.model_state_path, 'w'))
         torch.save(self.net, self.__last_model_path__())
 
@@ -72,19 +74,22 @@ def batch_generator(learning_point_generator, batch_size):
 
     return np.array(batch_features), np.float32(batch_target)
 
-def get_iterator(n_steps):
+def get_iterator(start, n_steps):
     if n_steps == 0:
-        i = 0
+        i = start
         while True:
             yield i
             i += 1
 
     else:
-        for i in xrange(n_steps):
+        for i in xrange(start, start+n_steps):
             yield i
 
 def main():
     parser = argparse.ArgumentParser(description="Train U-net")
+
+    parser.add_argument('--name', type=str, default='unknown',
+                        help='network name')
 
     parser.add_argument('--model_dir', type=str, required=True,
                         help='Where network will be saved and restored')
@@ -115,7 +120,7 @@ def main():
                         default=0,
                         help="Iteration's number. 0 mean inf")
 
-    parser.add_argument("--daset_dir",
+    parser.add_argument("--dataset_dir",
                         type=str,
                         default="../dataset/trainset")
 
@@ -135,6 +140,7 @@ def main():
 
     args = parser.parse_args()
 
+    net_name = args.name
     model_dir = args.model_dir
     learning_rate = args.lr
     batch_size = args.batch_size
@@ -145,25 +151,28 @@ def main():
     pretrained_vgg = args.pretrained_vgg == 'yes'
     fix_vgg = args.fix_vgg == 'yes'
 
+    print("Load dataset")
     dataset = DS.DataSet(dataset_dir)
 
     train = dataset.train_images()
     test = dataset.test_images()
 
-    network_manager = NManager(model_dir)
+    print("Initialize network manager")
+    network_manager = NManager(model_dir, net_name)
     if network_manager.registered:
         net = network_manager.get_net()
     else:
         net = U.Unet(vgg_pretrained=pretrained_vgg)
+        network_manager.register_net(net)
 
-
+    print("Move to GPU")
     net.cuda()
 
     def get_features(x):
         return x.get_ndarray([DS.ChannelRGB_PanSharpen])
 
     def get_target(x):
-        return x.get_mask()
+        return x.get_interior_mask()
 
     learning_point_generator = BG.LearningPointGenerator(train, get_features, get_target,
                                                          input_size, input_size - 208, rotate_amplitude=10,
@@ -175,23 +184,29 @@ def main():
         parameters = net.parameters()
 
     optimizer = torch.optim.Adam(parameters, lr=learning_rate)
-    logger = SummaryWriter(tb_log_dir)
+    logger = SummaryWriter(tb_log_dir + "/" + net_name)
 
-    for _ in tqdm.tqdm(get_iterator(iteration_n)):
-        iteration = network_manager.iteration
-        batch_features, batch_target = batch_generator(learning_point_generator, batch_size)
+    print("Start learning")
+    try:
+        for _ in tqdm.tqdm(get_iterator(network_manager.iteration, iteration_n)):
+            iteration = network_manager.iteration
+            batch_features, batch_target = batch_generator(learning_point_generator, batch_size)
+            
+            batch_features = Variable(FloatTensor(batch_features)).cuda()
+            batch_target = Variable(FloatTensor(batch_target)).cuda()
 
-        batch_features = Variable(FloatTensor(batch_features))
-        batch_target = Variable(FloatTensor(batch_target))
+            predicted = net.forward(batch_features)
 
-        predicted = net.forward(batch_features)
+            loss = F.binary_cross_entropy(predicted, batch_target)
 
-        loss = F.binary_cross_entropy(predicted, batch_target)
+            loss.backward()
+            optimizer.step()
 
-        loss.backward()
-        optimizer.step()
+            logger.add_scalar('loss', loss.data[0], iteration)
+            network_manager.iteration += 1
 
-        logger.add_scalar('loss', loss)
+    except BaseException as e:
+        network_manager.save()
 
 if __name__ == "__main__":
     main()
