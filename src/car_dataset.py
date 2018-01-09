6e43d1a855e0f021568c60dabe435503798de969
Dmitry Vyal <dmitryvyal@gmail.com>
2018-01-09 21:53:51 +0300

    CarDataset

---
+++ src/car_dataset.py
@@ -0,0 +1,24 @@
+import os
+
+import numpy as np
+import PIL.Image as Img
+from torch.utils.data.dataset import Dataset
+
+
+class CarDataset(Dataset):
+    def __init__(self, path):
+        self.imgs_path = os.path.join(path, 'images_prepped_train')
+        self.anns_path = os.path.join(path, 'annotations_prepped_train')
+
+        self.imgs_files = os.listdir(self.imgs_path)
+
+    def __len__(self):
+        return len(self.imgs_files)
+
+    def __getitem__(self, i):
+        img_path = os.path.join(self.imgs_path, self.imgs_files[i])
+        ann_path = os.path.join(self.anns_path, self.imgs_files[i])
+        img = Img.open(img_path)
+        ann = Img.open(ann_path)
+
+        return np.array(img), np.array(ann)
